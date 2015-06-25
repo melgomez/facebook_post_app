f8ebabf7c73316cf4d8825e48ceb4bcc734e43c0
melgomez <meldarrelgomez@gmail.com>
2015-06-25 19:26:59 +0800

    Clean redirect code for confirm logged in function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def confirm_logged_in
     unless session[:user_id]
       flash[:notice] = "Please log in."
-      redirect_to ({:controller => 'home', :action => 'index'})
+      redirect_to root_url
       return false
     else
       return true
