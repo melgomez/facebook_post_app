b2014a3de6f379dc7017a8dd8ad891f9b4bcf176
melgomez <meldarrelgomez@gmail.com>
2015-06-29 00:56:15 +0800

    Establish association relationships between models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create.tap do |user|
       user.provider = auth.provider
