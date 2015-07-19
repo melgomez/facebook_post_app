8d1f8d74201a8b361fbab49a5c0410cbe21a57b4
melgomez <meldarrelgomez@gmail.com>
2015-07-19 20:47:00 +0800

    Fix bug: change single post publish schedule to multiple job entries.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,15 +8,17 @@ class Post < ActiveRecord::Base
   scope :less_than_five, lambda{ where publish_date: (Time.now - 30.days)..(Time.now + 5.minutes) }
   
   def self.next_for_publish
-    Post.publish_next_queue.unpublished.less_than_five.first
+    Post.publish_next_queue.unpublished.less_than_five
   end
   
   def self.publish
-    post = Post.next_for_publish
+    posts = Post.next_for_publish
     if post.nil?
       return false
     else
-      PostWorker.perform_at(post.publish_date, post.id)
+      post.each do |post|
+        PostWorker.perform_at(post.publish_date, post.id)
+      end
     end
   end
 end
